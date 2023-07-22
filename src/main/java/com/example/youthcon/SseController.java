32ef38f4616465ee79e6113a7760e4bc81868dfa
Bue-von-hon <dkssudvn2@gmail.com>
2023-07-22 14:18:27 +0900

    Modification: - index.html: 현재 탭에서 아티클을 변경한 경우, 새로운 커넥션 요청을 보내게 변경 - Connection.java: emitter를 쓰레드-세이프하게 다루기 위한 클래스, tab id를 이용해 동등성 및 동일성 보장 - SseController.java: 불필요한 메소드 삭제 - SseEmitters.java: 명시적 락을 활용해 공유변수에 접근하도록 수정, emitter를 직접 사용하는 대신 connection을 사용하게 수정, 공통으로 사용하는 로직 메소드로 추출, 캐시가 만료되어도 그대로 남아있는 문제를 명시적으로 cleanup하여 해결

--- src/main/java/com/example/youthcon/SseController.java
+++ src/main/java/com/example/youthcon/SseController.java
@@ -1,11 +1,10 @@
 package com.example.youthcon;
 
-import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,41 +20,18 @@ public class SseController {
         this.sseEmitters = sseEmitters;
     }
 
-    // todo(hun): 한명의 유저가 하나의 브라우저에 여러 탭을 실행중인 경우만 가정, 아티클도 하나
     @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<SseEmitter> connect(@RequestParam("tabId") final String tabId,
                                               @RequestParam("articleId") final String articleId) {
         log.info("tabId : {}", tabId);
-        final SseEmitter emitter = sseEmitters.connect(tabId, articleId);
-        try {
-            final SseEmitter.SseEventBuilder eventBuilder = SseEmitter.event()
-                                                                      .name("connect")
-                                                                      .data("connected!")
-                                                                      .reconnectTime(10000L);
-            emitter.send(eventBuilder);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return ResponseEntity.ok(emitter);
+        final Connection connection = sseEmitters.connect(tabId, articleId);
+        connection.connect();
+        return ResponseEntity.ok(connection.getEmitter());
     }
 
     @PostMapping("/count")
     public ResponseEntity<Void> count(@RequestParam("articleId") final String articleId) {
         sseEmitters.count(articleId);
         return ResponseEntity.ok().build();
-    }
-
-    @PatchMapping("/tarticle")
-    public void changeTaticle(@RequestParam("tabid") final String tabId,
-                              @RequestParam("newarticleid") final String newArticleId,
-                              @RequestParam("oldarticleid") final String oldArticleId) {
-        sseEmitters.changeTaticle(tabId, newArticleId, oldArticleId);
-    }
-
-    @PostMapping("/disconnect")
-    public void disconnect(@RequestParam("tabId") final String tabId,
-                           @RequestParam("articleId") final String articleId) {
-        log.info("cleanup called! tab's id : {}", tabId);
-        sseEmitters.disconnect(tabId, articleId);
     }
 }
--- src/main/java/com/example/youthcon/SseEmitters.java
+++ src/main/java/com/example/youthcon/SseEmitters.java
@@ -1,21 +1,21 @@
 package com.example.youthcon;
 
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReentrantLock;
 
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import com.google.errorprone.annotations.concurrent.GuardedBy;
 
 @Component
 @Slf4j
@@ -24,70 +24,60 @@ public class SseEmitters {
     // todo(hun): 아티클별 조회수
     private static final Map<String, AtomicLong> articleId2AtomicLong = new ConcurrentHashMap<>();
 
-    private final LoadingCache<String, SseEmitter> tabId2SseEmitter = CacheBuilder.newBuilder()
-                                                                    .expireAfterAccess(10000L, TimeUnit.MILLISECONDS)
-                                                                    .build(
-                                                                            new CacheLoader<String, SseEmitter>() {
-                                                                                @Override
-                                                                                public SseEmitter load(String key) {
-                                                                                    // 캐시 항목이 없거나 만료된 경우에 값을 로드하는 코드
-                                                                                    return null;
-                                                                                }
-                                                                            }
-                                                                    );
+    @GuardedBy("lock")
+    private final Cache<String, Connection> tabIdToConnection = CacheBuilder.newBuilder()
+                                                                            .expireAfterAccess(10000L, TimeUnit.MILLISECONDS)
+                                                                            .build();
 
-    private final Map<String, Set<SseEmitter>> articleToEmitter = new ConcurrentHashMap<>();
+    @GuardedBy("lock")
+    private final Map<String, Set<Connection>> articleToConnection = new ConcurrentHashMap<>();
+    private final ReentrantLock lock = new ReentrantLock();
 
-    public SseEmitter connect(final String tabId, final String articleId) {
-        final SseEmitter oldEmitter = tabId2SseEmitter.getIfPresent(tabId);
+    public Connection connect(final String tabId, final String articleId) {
+        final Connection oldConnection = tabIdToConnection.getIfPresent(tabId);
 
-        if (oldEmitter != null) {
-            oldEmitter.complete();
+        if (oldConnection != null) {
+            oldConnection.complete();
         }
 
         final SseEmitter newEmitter = new SseEmitter(10000L);
-        setCallback(articleId, newEmitter);
-        tabId2SseEmitter.put(tabId, newEmitter);
+        final Connection newConnection = new Connection(newEmitter, tabId);
+        setCallback(articleId, newConnection);
 
-        final Set<SseEmitter> connections = articleToEmitter.getOrDefault(articleId, new HashSet<>());
-        connections.add(newEmitter);
-        articleToEmitter.put(articleId, connections);
+        lock.lock();
+        try {
+            tabIdToConnection.put(tabId, newConnection);
+            final Set<Connection> connections = articleToConnection.getOrDefault(articleId, new HashSet<>());
+            connections.add(newConnection);
+            articleToConnection.put(articleId, connections);
+            log.info("emitter list size: {}", tabIdToConnection.size());
+            log.info("emitter : {}", connections);
+        } finally {
+            lock.unlock();
+        }
 
-        log.info("emitter list size: {}", tabId2SseEmitter.size());
-        log.info("emitter : {}", newEmitter);
-        return newEmitter;
+        return newConnection;
     }
 
-    private void setCallback(final String articleId, final SseEmitter emitter) {
-        emitter.onCompletion(() -> {
-            synchronized (articleToEmitter) {
-                final Set<SseEmitter> emitters = articleToEmitter.get(articleId);
-                if (emitters != null && !emitters.isEmpty()) {
-                    emitters.remove(emitter);
-                    articleToEmitter.put(articleId, emitters);
-                }
-            }
-        });
+    private void setCallback(final String articleId, final Connection connection) {
+        connection.onCompletion(() -> deleteConnectionFromArticleToConnection(articleId, connection));
+        connection.onError(error -> deleteConnectionFromArticleToConnection(articleId, connection));
+        connection.onTimeout(() -> deleteConnectionFromArticleToConnection(articleId, connection));
+    }
 
-        emitter.onError(error -> {
-            synchronized (articleToEmitter) {
-                final Set<SseEmitter> emitters = articleToEmitter.get(articleId);
-                if (emitters != null && !emitters.isEmpty()) {
-                    emitters.remove(emitter);
-                    articleToEmitter.put(articleId, emitters);
-                }
+    private void deleteConnectionFromArticleToConnection(String articleId, Connection connection) {
+        lock.lock();
+        try {
+            final Set<Connection> connections = articleToConnection.getOrDefault(articleId, new HashSet<>());
+            if (!connections.isEmpty()) {
+                connections.remove(connection);
+                articleToConnection.put(articleId, connections);
+                tabIdToConnection.cleanUp();
             }
-        });
 
-        emitter.onTimeout(() -> {
-            synchronized (articleToEmitter) {
-                final Set<SseEmitter> emitters = articleToEmitter.get(articleId);
-                if (emitters != null && !emitters.isEmpty()) {
-                    emitters.remove(emitter);
-                    articleToEmitter.put(articleId, emitters);
-                }
-            }
-        });
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void count(final String articleId) {
@@ -95,53 +85,7 @@ public class SseEmitters {
         final long count = counter.incrementAndGet();
         articleId2AtomicLong.put(articleId, counter);
 
-        final Set<SseEmitter> emitters = articleToEmitter.getOrDefault(articleId, new HashSet<>());
-        emitters.forEach(emitter -> {
-            try {
-                final SseEmitter.SseEventBuilder eventBuilder = SseEmitter.event()
-                                                                          .name("count")
-                                                                          .data(count)
-                                                                          .reconnectTime(10000L);
-                emitter.send(eventBuilder);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    // 아티클을 보고 있던 탭에서 다른 아티클로 넘어갔다는 요청이 전송된 경우
-    public void changeTaticle(final String tabId, final String newArticleId, final String oldArticleId) {
-        final SseEmitter oldEmitter = tabId2SseEmitter.getIfPresent(tabId);
-        if (oldEmitter == null) {
-            return;
-        }
-
-        final Set<SseEmitter> oldEmitters = articleToEmitter.getOrDefault(oldArticleId, new HashSet<>());
-        oldEmitters.remove(oldEmitter);
-        articleToEmitter.put(oldArticleId, oldEmitters);
-
-        final Set<SseEmitter> newEmitters = articleToEmitter.getOrDefault(newArticleId, new HashSet<>());
-        newEmitters.add(oldEmitter);
-        articleToEmitter.put(newArticleId, newEmitters);
-
-        log.info("tabId : {}", tabId);
-        log.info("previousArticleId : {}", oldArticleId);
-        log.info("articleId : {}", newArticleId);
-    }
-
-    public void disconnect(final String tabId, final String articleId) {
-        cleanup(tabId, articleId);
-    }
-
-    private void cleanup(String tabId, String articleId) {
-        final SseEmitter oldEmitter = tabId2SseEmitter.getIfPresent(tabId);
-        final Set<SseEmitter> emitters = articleToEmitter.get(articleId);
-        emitters.remove(oldEmitter);
-        articleToEmitter.put(articleId, emitters);
-        // 이전에 생성된 커넥션 완료 처리
-        if (oldEmitter != null) {
-            oldEmitter.complete();
-            tabId2SseEmitter.invalidate(tabId);
-        }
+        final Set<Connection> connections = articleToConnection.getOrDefault(articleId, new HashSet<>());
+        connections.forEach(connection -> connection.count(count));
     }
 }
